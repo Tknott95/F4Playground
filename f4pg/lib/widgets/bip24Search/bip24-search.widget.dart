e22fa110961c4c9687a737e9ec6ccfca40ce7883
tknott95 <tknott95@hotmail.com>
2022-11-16 18:50:26 -0700

    nested BipSingle - passing down index

--- f4pg/lib/widgets/bip24Search/bip24-search.widget.dart
+++ f4pg/lib/widgets/bip24Search/bip24-search.widget.dart
@@ -8,7 +8,7 @@ class Bip24SearchWidget extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
      children: [
       Text("bip24SearchWidget"),
-      BipSearchSingle(),
+      BipSearchSingle(bipIndex: 1),
       // Padding(
       //   padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
       //     child: TextFormField(
@@ -26,14 +26,18 @@ class Bip24SearchWidget extends StatelessWidget {
 
 class BipSearchSingle extends StatelessWidget {
 
+  int bipIndex;
+
+  BipSearchSingle({required this.bipIndex});
+
   @override
   Widget build(BuildContext context) {
   return Padding(
     padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
       child: TextFormField(
-        decoration: const InputDecoration(
+        decoration: InputDecoration(
           border: UnderlineInputBorder(),
-          labelText: 'bip 01',
+          labelText: 'bip $bipIndex',
         ),
       ),
     );
