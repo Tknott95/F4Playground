f4dc93b14a066b519ac7dc818821043e89d666a2
tknott95 <tknott95@hotmail.com>
2022-11-16 18:46:37 -0700

    BipSearchSingle - Init()

--- f4pg/lib/widgets/bip24Search/bip24-search.widget.dart
+++ f4pg/lib/widgets/bip24Search/bip24-search.widget.dart
@@ -4,6 +4,38 @@ class Bip24SearchWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("bip24SearchWidget");
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+     children: [
+      Text("bip24SearchWidget"),
+      BipSearchSingle(),
+      // Padding(
+      //   padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      //     child: TextFormField(
+      //       decoration: const InputDecoration(
+      //         border: UnderlineInputBorder(),
+      //         labelText: 'bip 01',
+      //       ),
+      //     ),
+      //   ),
+     ],
+    );
+    
+  }
+}
+
+class BipSearchSingle extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      child: TextFormField(
+        decoration: const InputDecoration(
+          border: UnderlineInputBorder(),
+          labelText: 'bip 01',
+        ),
+      ),
+    );
   }
 }
