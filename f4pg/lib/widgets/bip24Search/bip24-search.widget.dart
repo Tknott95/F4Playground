73dc732b45f70d91c4dba0dd348e5dc8c273255c
tknott95 <tknott95@hotmail.com>
2022-11-16 18:37:48 -0700

    bip24SearchWidget - INIT()

---
+++ f4pg/lib/widgets/bip24Search/bip24-search.widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+  
+class Bip24SearchWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("bip24SearchWidget");
+  }
+}
