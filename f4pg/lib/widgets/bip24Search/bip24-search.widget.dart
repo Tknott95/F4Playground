ac871dc4cc8b4a485006afae1c0c98002780b1f2
tknott95 <tknott95@hotmail.com>
2022-11-16 18:58:00 -0700

    bip rec

--- f4pg/lib/widgets/bip24Search/bip24-search.widget.dart
+++ f4pg/lib/widgets/bip24Search/bip24-search.widget.dart
@@ -5,15 +5,28 @@ class Bip24SearchWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
+     crossAxisAlignment: CrossAxisAlignment.center,
      children: [
       Text("bip24SearchWidget"),
       /* create row type system with dynamic flex box type behaviour and use for loop here after bip search filter */
       /* I want to lock the input with a lock to lock bip or change it for rec (this or use breadcrumbs after input) */
-      BipSearchSingle(bipIndex: 1),
-      BipSearchSingle(bipIndex: 2),
-      BipSearchSingle(bipIndex: 3),
-      BipSearchSingle(bipIndex: 4),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BipSearchSingle(bipIndex: 1),
+          BipSearchSingle(bipIndex: 2),
+          BipSearchSingle(bipIndex: 3),
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BipSearchSingle(bipIndex: 4),
+          BipSearchSingle(bipIndex: 5),
+          BipSearchSingle(bipIndex: 6),
+        ],
+      )
+
 
       // Padding(
       //   padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
@@ -38,16 +51,19 @@ class BipSearchSingle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  return Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-      child: TextField(
-        /* not using TextFormField for now so I can utilize enabled */
-        enabled: true, /* add lock with bool for this so you can only edit one at a time */
-        decoration: InputDecoration(
-          border: UnderlineInputBorder(),
-          labelText: 'bip $bipIndex',
+  return SizedBox(
+    width: 300,
+    child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+        child: TextField(
+          /* not using TextFormField for now so I can utilize enabled */
+          enabled: true, /* add lock with bool for this so you can only edit one at a time */
+          decoration: InputDecoration(
+            border: UnderlineInputBorder(),
+            labelText: 'bip $bipIndex',
+          ),
         ),
       ),
-    );
+  );
   }
 }
