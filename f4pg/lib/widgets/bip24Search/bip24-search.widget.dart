f2fc5371d540309fd872470d1c33ab80009ad129
tknott95 <tknott95@hotmail.com>
2022-11-16 19:44:38 -0700

    bips - list under for filter search prepped

--- f4pg/lib/widgets/bip24Search/bip24-search.widget.dart
+++ f4pg/lib/widgets/bip24Search/bip24-search.widget.dart
@@ -11,8 +11,8 @@ class Bip24SearchWidget extends StatelessWidget {
       /* create row type system with dynamic flex box type behaviour and use for loop here after bip search filter */
       /* I want to lock the input with a lock to lock bip or change it for rec (this or use breadcrumbs after input) */
       Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+       mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
           BipSearchSingle(bipIndex: 1),
           BipSearchSingle(bipIndex: 2),
           BipSearchSingle(bipIndex: 3),
@@ -46,11 +46,18 @@ class Bip24SearchWidget extends StatelessWidget {
   }
 }
 
-class BipSearchSingle extends StatelessWidget {
+class BipSearchSingle extends StatefulWidget {
+  final int bipIndex;
 
-  int bipIndex;
+  const BipSearchSingle({required this.bipIndex});
 
-  BipSearchSingle({required this.bipIndex});
+  State<BipSearchSingle> createState() => _BipSearchSingleState();
+}
+
+class _BipSearchSingleState extends State<BipSearchSingle> {
+
+
+  // BipSearchSingle({required this.bipIndex});
 
   void searchBipList(String val) {
     
@@ -58,19 +65,71 @@ class BipSearchSingle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  return SizedBox(
-    width: 300,
-    child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-        child: TextField(
-          /* not using TextFormField for now so I can utilize enabled */
-          enabled: true, /* add lock with bool for this so you can only edit one at a time */
-          decoration: InputDecoration(
-            border: UnderlineInputBorder(),
-            labelText: 'bip $bipIndex',
+    return SizedBox(
+      width: 300,
+      child: Column(
+        children: [
+          SizedBox(
+            width: 300,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextField(
+                  /* not using TextFormField for now so I can utilize enabled */
+                  enabled: true, /* add lock with bool for this so you can only edit one at a time */
+                  decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'bip ${widget.bipIndex}',
+                  ),
+                ),
+              ),
+            ),
+            
+          SizedBox(
+            height: 50, // constrain height
+            child: ListView(
+              shrinkWrap: true,
+              children: const [
+                Text("HAHLOASDLKASLSHJK"),
+                Text("HAHLOASDLKASLHJK"),
+                Text("HAHLOASDLKASLHJK"),
+                Text("HAHLOASDLKASLHJK"),
+                Text("HAHLOASDLKASLHJK"),
+              ],
+            ),
           ),
-        ),
+
+
+        //   SingleChildScrollView(
+        //   scrollDirection: Axis.horizontal,
+        //   child: Row(
+        //   children: <Widget>[
+        //     Text('hi'),
+        //     Text('hi'),
+        //     Text('hi'),
+        //   ]
+        //   )
+        // )
+
+          
+            // Expanded(
+            //   child: ListView(
+            //     shrinkWrap: true,
+            //     children: [
+            //       Center(
+            //         child: Text('Entry A')
+            //       ),
+            //     ],
+            //   )
+            // )
+        ],
       ),
-  );
-  }
+    );
+   }
+}
+
+
+class BipStringModel{
+  String? val;
+
+  BipStringModel(this.val);
 }
