9c6a538486f7245d2ed70b706cc152c903059484
Trevor Knott <Tknott95@hotmail.com>
2022-11-26 00:03:41 -0800

    basic foundation

--- f4pg/lib/pages/wallet_login/wallet_login.pages.dart
+++ f4pg/lib/pages/wallet_login/wallet_login.pages.dart
@@ -7,10 +7,40 @@ class WalletLoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child:  Column(
-        children: [
-          Bip24SearchWidget(),
-        ],
+      child: Card(
+        // color: const Color.fromARGB(200, 1, 1, 1),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 200,
+              child: TextFormField(
+                initialValue: 'wallet name',
+              ),
+            ),
+            SizedBox(
+              height: 200,
+              child: Bip24SearchWidget()
+            ),
+            SizedBox(
+              width: 200,
+              child: TextFormField(
+                initialValue: 'wallet password',
+              ),
+            ),
+
+             SizedBox(
+              height: 200,
+              width: 400,
+              child: MaterialButton(
+                child: Text('recover wallet'),
+                onPressed: () {
+                  print('fire at node');
+                },
+              ),
+            ),
+          ],
+        ),
       )
     );
   }
