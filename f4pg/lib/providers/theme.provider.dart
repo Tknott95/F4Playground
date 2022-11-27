e4bb74931d5b182c7a84798123953702bf12cd6e
tknott95 <tknott95@hotmail.com>
2022-11-27 13:40:59 -0700

    builder wrappes -  theme provider bind to theme -WORKING

--- f4pg/lib/providers/theme.provider.dart
+++ f4pg/lib/providers/theme.provider.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ThemeProvider extends ChangeNotifier {
   
   /* the goal is to create a custom theming page where we et values here to change the whole app */
-  ThemeMode _themeMode = ThemeMode.dark; /* starting theme */
+  ThemeMode _themeMode = ThemeMode.light; /* starting theme */
 
   ThemeMode get themeMode => _themeMode;
   bool get isDarkMode => _themeMode == ThemeMode.dark;
