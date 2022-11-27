209c559678ab1df8a8b747fa4112992d7ff757d9
tknott95 <tknott95@hotmail.com>
2022-11-27 13:17:02 -0700

    theming prep

---
+++ f4pg/lib/providers/theme.provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  
+  /* the goal is to create a custom theming page where we et values here to change the whole app */
+  ThemeMode _themeMode = ThemeMode.dark;
+
+  bool get isDarkMode => _themeMode == ThemeMode.dark;
+
+  void changeThemeMode() {
+    isDarkMode ? _themeMode = ThemeMode.light :  _themeMode = ThemeMode.dark;
+     notifyListeners();
+  }
+
+}
