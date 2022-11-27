f6a88679bf5026c3626fe235e13b152b75cdf76f
tknott95 <tknott95@hotmail.com>
2022-11-27 09:40:27 -0700

    WalletLoginProvider

--- f4pg/lib/providers/wallet_login.provider.dart
+++ f4pg/lib/providers/wallet_login.provider.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 /* refactor this into a type */
 class WalletLoginProvider extends ChangeNotifier {
-  List<String> Bips24Keys = [];
+  List<String> _bips24Keys = [];
 
-  void setBip24Keys(List<String> _bips24Keys) {
-    print('  BEFORE Bips24Keys: $Bips24Keys');
-    Bips24Keys = _bips24Keys;
-    print('  AFTER Bips24Keys: $Bips24Keys');
+  List<String> get getBips24Keys => _bips24Keys;
+
+  void setBip24Keys(List<String> bips24Keys) {
+    print('  BEFORE Bips24Keys: $_bips24Keys');
+    _bips24Keys = bips24Keys;
+    print('  AFTER Bips24Keys: $_bips24Keys');
   
     notifyListeners();
   }
